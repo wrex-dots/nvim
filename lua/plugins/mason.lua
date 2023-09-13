7deed08cd90108cb3db18100850b732d1cae86bc
Ludovic Fernandez <ludofernandez@msn.com>
2023-09-13 13:09:07 +0200

    [Mason]: Use Lua LS's snippet for NeoVim-aware LSP setup

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -34,8 +34,8 @@ return {
 		},
 
 		config = function()
-			local lspconfig = require("lspconfig")
 			local masonlsp = require("mason-lspconfig")
+			local lspconfig = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			masonlsp.setup({
@@ -67,6 +67,45 @@ return {
 				handlers = {
 					function(name)
 						lspconfig[name].setup({ capabilities = capabilities })
+					end,
+					["lua_ls"] = function()
+						lspconfig.lua_ls.setup({
+							capabilities = capabilities,
+							on_init = function(client)
+								local path = client.workspace_folders[1].name
+								if
+									not vim.loop.fs_stat(path .. "/.luarc.json")
+									and not vim.loop.fs_stat(path .. "/.luarc.jsonc")
+								then
+									client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+										Lua = {
+											runtime = {
+												-- Tell the language server which version of Lua you're using
+												-- (most likely LuaJIT in the case of Neovim)
+												version = "LuaJIT",
+											},
+											-- Make the server aware of Neovim runtime files
+											workspace = {
+												checkThirdParty = false,
+												library = {
+													vim.env.VIMRUNTIME,
+													-- "${3rd}/luv/library"
+													-- "${3rd}/busted/library",
+												},
+												-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+												-- library = vim.api.nvim_get_runtime_file("", true)
+											},
+										},
+									})
+
+									client.notify(
+										"workspace/didChangeConfiguration",
+										{ settings = client.config.settings }
+									)
+								end
+								return true
+							end,
+						})
 					end,
 					["tsserver"] = function()
 						local language_settings = {
