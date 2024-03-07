a0e2e30212269731ec6a6862f733fbd408c15d16
Ludovic Fernandez <ludofernandez@msn.com>
2024-03-07 18:01:29 +0100

    fix(foxutils.fs): `current_file` methods returning its own module's paths when used in scripts

--- lua/foxutils/fs.lua
+++ lua/foxutils/fs.lua
@@ -1,19 +1,32 @@
 local M = {}
+local fs = vim.fs
+
+local HERE = debug.getinfo(1, "S").source:gsub("^@", "")
+
 M.current_file = {}
 
 function M.current_file.name()
-  local path = debug.getinfo(1, "S").source:gsub("^\\@", "")
-  return vim.fs.basename(path)
+  local path = debug.getinfo(1, "S").source:gsub("^@", "")
+
+  if path == HERE then path = debug.getinfo(2, "S").source:gsub("^@", "") end
+
+  return fs.normalize(fs.basename(path))
 end
 
 function M.current_file.dirname()
-  local path = debug.getinfo(1, "S").source:gsub("^\\@", "")
-  return vim.fs.dirname(path)
+  local path = debug.getinfo(1, "S").source:gsub("^@", "")
+
+  if path == HERE then path = debug.getinfo(2, "S").source:gsub("^@", "") end
+
+  return fs.normalize(fs.dirname(path))
 end
 
 function M.current_file.path()
   local path = debug.getinfo(1, "S").source:gsub("^@", "")
-  return vim.fs.normalize(path)
+
+  if path == HERE then path = debug.getinfo(2, "S").source:gsub("^@", "") end
+
+  return fs.normalize(path)
 end
 
 return M
