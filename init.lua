dd8c8162bcc92cf70e1d05d07f2d00a9ee7c5bd8
Ludovic Fernandez <ludofernandez@msn.com>
2024-03-08 16:45:11 +0100

    chore(init): Remove TODOs display on startup

--- init.lua
+++ init.lua
@@ -3,16 +3,16 @@ local luaconfd = vim.fn.stdpath("config") .. "/lua/config"
 
 --- Get all files recursively
 local luaconfs = vim.fs.find(function(name)
-	return name:match(".*%.lua")
+  return name:match(".*%.lua")
 end, {
-	type = "file",
-	path = luaconfd,
-	limit = math.huge,
+  type = "file",
+  path = luaconfd,
+  limit = math.huge,
 })
 
 -- Source all files fetched into luaconfs
 for _, conf in ipairs(luaconfs) do
-	require("config." .. vim.fs.basename(conf):gsub("%.lua$", ""))
+  require("config." .. vim.fs.basename(conf):gsub("%.lua$", ""))
 end
 
 -- .vim configuration files directory
@@ -20,21 +20,17 @@ local vimconfd = vim.fn.stdpath("config") .. "/vim.d"
 
 -- Get all the .vim files from vimconfd recursively
 local vimfiles = vim.fs.find(function(name)
-	return name:match(".*%.vim")
+  return name:match(".*%.vim")
 end, {
-	type = "file",
-	path = vimconfd,
-	limit = math.huge,
+  type = "file",
+  path = vimconfd,
+  limit = math.huge,
 })
 
 -- Source all files fetched into vimfiles
 for _, conf in ipairs(vimfiles) do
-	vim.cmd.source(conf)
+  vim.cmd.source(conf)
 end
 
--- Install LazyVim if necessary, then install and configure all plugins
--- defined in lua/plugins
+-- Install LazyVim if necessary, then install all configured plugins
 require("lazyconfig")
-
--- Show todos when entering NeoVim
-require("foxutils.todo")
