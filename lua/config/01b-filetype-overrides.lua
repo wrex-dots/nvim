dcf299cfff5f824d953bec48f3025e04989c15c2
Ludovic Fernandez <ludofernandez@msn.com>
2024-03-08 17:33:24 +0100

    refactor(ft): Add a config to create new filetypes, run it before the overrides

--- lua/config/01b-filetype-overrides.lua
+++ lua/config/01b-filetype-overrides.lua
@@ -10,30 +10,30 @@
 
 ---@type Array<FtOverride>
 local overrides = {
-	{
-		"*.map",
-		"json",
-	},
-	{
-		"*.json",
-		"jsonc",
-	},
-	{
-		"\\cdockerfile",
-		"dockerfile",
-	},
-	{
-		".envrc",
-		"sh",
-	},
+  {
+    "*.map",
+    "json",
+  },
+  {
+    "*.json",
+    "jsonc",
+  },
+  {
+    "\\cdockerfile",
+    "dockerfile",
+  },
+  {
+    ".envrc",
+    "sh",
+  },
 }
 
 local augroup = vim.api.nvim_create_augroup("FileTypeOverrides", {})
 
 for _, override in ipairs(overrides) do
-	vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-		group = augroup,
-		pattern = override[1],
-		command = "set ft=" .. override[2],
-	})
+  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    group = augroup,
+    pattern = override[1],
+    command = "set ft=" .. override[2],
+  })
 end
