08a95d8ce7446da07188f7747bbafe4618e22281
Ludovic Fernandez <ludofernandez@msn.com>
2024-03-08 15:20:10 +0100

    feat(UI): Add scrollbars
    There's a switch to check if nvim0.10.x is running, as this version gets a better plugin.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -60,4 +60,19 @@ table.insert(M, { -- More informative search UI
   keys = { "/", "?" },
 })
 
+local use_scrollview = vim.fn.has("nvim-0.10") == 0
+table.insert(M, { -- Fancy scroll bars
+  {
+    "lewis6991/satellite.nvim",
+    cond = not use_scrollview,
+    event = "VeryLazy",
+  },
+  {
+    "dstein64/nvim-scrollview",
+    cond = use_scrollview,
+    event = "VeryLazy",
+    config = true,
+  },
+})
+
 return M
