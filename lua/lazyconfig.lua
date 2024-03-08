3468cf2df1c04a751a21bb6998b1cddc39c595dc
Ludovic Fernandez <ludofernandez@msn.com>
2024-03-08 15:06:34 +0100

    style: Format lazyconfig.lua

--- lua/lazyconfig.lua
+++ lua/lazyconfig.lua
@@ -1,25 +1,25 @@
-local fox = require('foxutils')
+local fox = require "foxutils"
 
 assert(
-  fox.systemhas('git'),
+  fox.systemhas "git",
   [[WTF?
   You do not have `git` in your PATH.
-  Lazy.nvim won't be installed, nor will any of the plugins (and awesome configs).]]
+  Lazy.nvim won't be installed, nor will any of the plugins and awesome configs.]]
 )
 
--- Installation path ($XDG_CONFIG_HOME/nvim/lazy)
-local lazypath = vim.fn.stdpath('config') .. '/lazy'
+-- Installation path (`$XDG_CONFIG_HOME/nvim/lazy`)
+local lazypath = vim.fn.stdpath "config" .. "/lazy"
 
 -- Automatically install LazyVim
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
     lazypath,
-  })
+  }
 end
 
 -- Make NeoVim aware of LazyVim
@@ -27,10 +27,10 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Not everything is written in here, if you want to know all the defaults that you can override,
 -- check here: https://github.com/folke/lazy.nvim
-require('lazy').setup {
+require("lazy").setup {
   -- Lua module to load specs from ($XDG_CONFIG_HOME/nvim/lua/plugins)
-  -- Will automatically merge all specs returned from plugins/*.lua
-  import = '/plugins',
+  -- Will automatically merge all specs returned from plugins/**/*.lua
+  import = "/plugins",
 
   -- Lazy load plugins by default
   lazy = true,
@@ -38,15 +38,15 @@ require('lazy').setup {
   -- Options for plugin devs
   -- You Surely want to change this to whatever dir contains your NeoVim plugin repos
   dev = {
-    path = '~/Repos/nvim/', -- Root to all your plugins
+    path = "~/Repos/nvim/", -- Root to all the plugins you dev
   },
 
   -- Cotrol installing plugins automatically
   install = {
     missing = true, -- If a plugin is missing on startup, install it
     colorscheme = { -- Try to load one of these color schemes during start-up install phase
-      'duskfox',
-      'habamax',
+      "duskfox",
+      "habamax",
     },
   },
 
@@ -56,10 +56,6 @@ require('lazy').setup {
   },
 }
 
-fox.keys.noremap.n("Open Lazy's control panel",
-  '<C-l>',
-  vim.cmd.Lazy,
-  {
-    silent = true,
-  }
-)
+fox.keys.noremap.n("Open Lazy's control panel", "<C-l>", vim.cmd.Lazy, {
+  silent = true,
+})
