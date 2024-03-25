418ca4fb114347dd313eed6fc16530ca197ce3d3
Ludovic Fernandez <ludofernandez@msn.com>
2024-03-25 15:18:03 +0100

    chore(ft): Add filtypes for rasi, kitty, mako, waybar

--- lua/config/01a-filetypes.lua
+++ lua/config/01a-filetypes.lua
@@ -1,5 +1,11 @@
 vim.filetype.add {
+  extension = {
+    ["rasi"] = "rasi",
+  },
   pattern = {
     [".*/hypr/.*%.conf"] = "hyprlang",
+    [".*/kitty/.*%.conf"] = "bash",
+    [".*/mako/config"] = "dosini",
+    [".*/waybar/config"] = "jsonc",
   },
 }
