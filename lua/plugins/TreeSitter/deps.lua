02998efb547dc750c5010d443bde778709086ba3
Ludovic Fernandez <ludofernandez@msn.com>
2024-03-25 15:29:17 +0100

    chore(plugins): Split TreeSitter configs

---
+++ lua/plugins/TreeSitter/deps.lua
@@ -0,0 +1,32 @@
+local deps = {
+  {
+    -- More context-aware refactoring
+    "nvim-treesitter/nvim-treesitter-refactor",
+
+    -- Manipulate and move around text objects
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+}
+
+table.insert(deps, {
+  -- Keep track of the current context as a sticky topline
+  "nvim-treesitter/nvim-treesitter-context",
+
+  opts = {
+    enable = true,
+    max_lines = 0,
+  },
+})
+
+table.insert(deps, {
+  -- Automatic bracket pairing
+  "windwp/nvim-autopairs",
+
+  event = "Syntax *",
+
+  opts = {
+    check_ts = true,
+  },
+})
+
+return deps
---
+++ lua/plugins/TreeSitter/extras/init.lua
@@ -0,0 +1,3 @@
+return {
+  require "plugins.TreeSitter.extras.tree-hopper",
+}
