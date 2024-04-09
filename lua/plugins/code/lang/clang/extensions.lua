ddeb1422ae2303bc545a92c2723be065bc0924ce
Ludovic Fernandez <ludofernandez@msn.com>
2024-04-09 18:53:54 +0200

    feat(Lang): Add C/CPP support

---
+++ lua/plugins/code/lang/clang/extensions.lua
@@ -0,0 +1,8 @@
+---@type LazyPluginSpec
+return {
+  "p00f/clangd_extensions.nvim",
+
+  ft = { "c", "cpp", "make", "cmake" },
+
+  module = true,
+}
---
+++ lua/plugins/code/lang/clang/lspconfig.lua
@@ -0,0 +1,38 @@
+local ensure = require "plugins.code.mason.ensure-installed"
+local server = "clangd"
+
+ensure["mason"]:add {
+  server,
+}
+
+---@type fun(T: LspTools): fun(client: lsp.Client, buffer: integer) : nil
+local function on_attach(T)
+  return function(client, buffer)
+    T.hook_fmt(client, buffer)
+
+    local ih = require "clangd_extensions.inlay_hints"
+    ih.setup_autocmd()
+    ih.set_inlay_hints()
+  end
+end
+
+---@type LspHandlerFactory
+local function factory(T)
+  return function()
+    vim.opt.shellpipe = "| tee build_log.sh"
+
+    local loaded, gt = pcall(require, "gentags")
+    if loaded then gt.setup() end
+
+    require("clangd_extensions").setup()
+
+    T.lspconfig.clangd.setup {
+      capabilities = T.capabilities,
+      on_attach = on_attach,
+    }
+  end
+end
+
+return {
+  [server] = factory,
+}
