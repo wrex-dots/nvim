b54fe0256e9511d843e734f252ded29601808e86
Ludovic Fernandez <ludofernandez@msn.com>
2024-04-16 13:39:45 +0200

    chore(lang/tags): Disable tags extension for now
    It's causing error messages on every single buffer write operation.

--- lua/plugins/code/mason/deps/ctags.lua
+++ lua/plugins/code/mason/deps/ctags.lua
@@ -5,6 +5,8 @@ local spec = {
   -- Automatic ctags generation
   "JMarkin/gentags.lua",
 
+  enabled = false, -- Getting errors on every single buffer write
+
   cond = has "ctags",
 
   dependencies = { "nvim-lua/plenary.nvim" },
