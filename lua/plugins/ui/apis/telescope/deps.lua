235f86da1857ad6f62009b7c67842b9370cf9fac
Ludovic Fernandez <ludofernandez@msn.com>
2024-06-27 20:16:47 +0200

    feat(tools.ui.apis.telescope): Add emoji picker

--- lua/plugins/ui/apis/telescope/deps.lua
+++ lua/plugins/ui/apis/telescope/deps.lua
@@ -7,8 +7,32 @@ local fzf = {
   "nvim-telescope/telescope-fzf-native.nvim",
 
   build = "make",
+}
+
+---@type LazyPluginSpec
+local emoji = {
+  "xiyaowong/telescope-emoji.nvim",
+
+  init = function()
+    ---@class EmojiDescription
+    ---@field name        string
+    ---@field value       string
+    ---@field category    string
+    ---@field description string
+
+    exts["emoji"] = {
+
+      ---@param e EmojiDescription
+      --[[ action = function(e)
+        -- insert emoji when picked
+        vim.api.nvim_put({ e.value }, "c", false, true)
+      end, ]]
+    }
+  end,
+}
 
 ---@type LazyPlugin[]
 return {
   fzf,
+  emoji,
 }
