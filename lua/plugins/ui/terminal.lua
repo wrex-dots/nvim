be07f818efe754ddd8e085b52a4a87b2bf717fe6
Ludovic Fernandez <ludofernandez@msn.com>
2024-07-03 01:34:29 +0200

    feat(ui.terminal): Add persistant toggleable terminal

---
+++ lua/plugins/ui/terminal.lua
@@ -0,0 +1,70 @@
+-- LazyVim
+
+---@type LazyPluginSpec
+local spec = {
+  "akinsho/toggleterm.nvim",
+
+  version = "*",
+
+  keys = function()
+    local fox = require "foxutils"
+    local function toggle(direction)
+      return function() vim.cmd.ToggleTerm("direction=" .. direction) end
+    end
+
+    return fox.keys.lazy({
+      {
+        "<F11>",
+        toggle "horizontal",
+        desc = "Open horizontal",
+      },
+      {
+        "<S-F11>",
+        toggle "float",
+        desc = "Open float",
+      },
+    }, {
+      prefix = "ToggleTerm: ",
+      mode = { "n", "v", "i", "t" },
+    })
+  end,
+
+  init = function()
+    vim.o.statusline = vim.o.statusline
+      .. '%{&ft == "toggleterm" ? "Terminal @".b:toggle_number : ""}'
+
+    vim.api.nvim_create_autocmd("TermOpen", {
+      desc = "Set Terminal mode keymaps",
+      pattern = "term://*",
+      group = vim.api.nvim_create_augroup(
+        "TermOpenSetKeymaps",
+        { clear = true }
+      ),
+      callback = function()
+        require("foxutils.keys").noremap.t(
+          "Terminal: Leave Terminal mode",
+          "<esc><esc>",
+          [[<C-\><C-n>]],
+          { buffer = 0 }
+        )
+      end,
+    })
+  end,
+
+  opts = function()
+    -- Check if user has defined $SHELL, use that one preferably.
+    -- Otherwise use Vim's shell option which should always have something.
+    local shell = vim.fn.expand "$SHELL"
+    shell = shell ~= "" and shell or vim.o.shell
+
+    return {
+      autochdir = true,
+      direction = "horizontal",
+      shell = shell,
+    }
+  end,
+
+  config = function(_, opts) require("toggleterm").setup(opts) end,
+}
+
+return spec
