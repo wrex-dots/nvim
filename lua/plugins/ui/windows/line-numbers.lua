4cfa3618a7234400193b7dcf58018e8b2f1573d6
Ludovic Fernandez <ludofernandez@msn.com>
2024-07-22 14:42:06 +0200

    refactor(ui.windows.line-numbers): Replace `gitgutter` with `gitsigns`
    Integrates with `barbar` (the tab bar plugin)... Well they say that but
    I saw no difference. I think `gitsigns` looks better anyway and I like
    its features more. `gitgutter` was not supposed to stay, but it was here
    because `gitsigns` wasn't working—turns out it was because `gitgutter`
    was enabled lol.

--- lua/plugins/ui/windows/line-numbers.lua
+++ lua/plugins/ui/windows/line-numbers.lua
@@ -2,28 +2,43 @@
 
 ---@type LazyPluginSpec[]
 local linenums = {
-  {
-    "airblade/vim-gitgutter",
+  "kennykaye/vim-relativity",
 
-    init = function() vim.g.gitgutter_highlight_linenrs = 1 end,
-  },
-  {
-    "kennykaye/vim-relativity",
+  init = function()
+    local ignored_fts = {
+      "NvimTree",
+      "toggleterm",
+      "dap-repl",
+    }
 
-    init = function()
-      local ignored_fts = {
-        "NvimTree",
-        "toggleterm",
-        "dap-repl",
-      }
-
-      vim.o.number = true
-      vim.o.relativenumber = true
-      vim.g.relativity_enabled_on_start = 1
-      vim.g.relativity_focus_toggle = 1
-      vim.g.relativity_filetype_ignore = ignored_fts
-    end,
-  },
+    vim.o.number = true
+    vim.o.relativenumber = true
+    vim.g.relativity_enabled_on_start = 1
+    vim.g.relativity_focus_toggle = 1
+    vim.g.relativity_filetype_ignore = ignored_fts
+  end,
 }
 
-return linenums
+---@type LazyPluginSpec[]
+local gitsigns = {
+  "lewis6991/gitsigns.nvim",
+
+  opts = {
+    numhl = true, -- Colorise line numbers according to their correspondig sign column item
+    current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+  },
+
+  config = function(_, opts)
+    require("gitsigns").setup(opts)
+
+    -- Enable current line blame by default
+    -- Will show an inlay hint next to the line telling you who made the change and when
+    -- after a short period of cursor inactivity
+    vim.cmd.Gitsigns "toggle_current_line_blame"
+  end,
+}
+
+return {
+  gitsigns,
+  linenums,
+}
