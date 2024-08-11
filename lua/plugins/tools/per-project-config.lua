8e70056c957dbba0a4158f4d36a971b04bfb51da
Ludovic Fernandez <ludofernandez@msn.com>
2024-08-11 09:35:40 +0200

    chore(tools.per-project-config): Set procject config dir to `~/.config/lua/project-config`

--- lua/plugins/tools/per-project-config.lua
+++ lua/plugins/tools/per-project-config.lua
@@ -1,5 +1,5 @@
 ---@type LazyPluginSpec
-return {
+local spec = {
   "windwp/nvim-projectconfig",
 
   lazy = false,
@@ -9,7 +9,16 @@ return {
   opts = {
     silent = false,
     autocmd = true,
+    project_dir = vim.fs.joinpath(
+      ---`stdpath('config')` returns a single string
+      ---@diagnostic disable-next-line: param-type-mismatch
+      vim.fn.stdpath "config",
+      "lua",
+      "project-config"
+    ),
   },
 
   config = true,
 }
+
+return spec
